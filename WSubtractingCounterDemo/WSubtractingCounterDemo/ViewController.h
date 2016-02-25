fbfae09232b841c4d0210b7181f76a852f70903e
weisheng.wang <weisheng.wang@alibaba-inc.com>
2016-02-25 21:12:17 +0800

    代码修改

---
+++ WSubtractingCounterDemo/WSubtractingCounterDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WSubtractingCounterDemo
+//
+//  Created by 王炜圣 on 16/2/25.
+//  Copyright © 2016年 weisheng.wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WSubtractingCounterDemo/WSubtractingCounterDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WSubtractingCounterDemo
+//
+//  Created by 王炜圣 on 16/2/25.
+//  Copyright © 2016年 weisheng.wang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
