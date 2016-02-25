0a0939a6a4eb0946b1f1b9300b61ede3c01aa773
weisheng.wang <weisheng.wang@alibaba-inc.com>
2016-02-25 13:03:25 +0800

    //工程提交

---
+++ AppCount/AppCount/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppCount
+//
+//  Created by 王炜圣 on 16/2/25.
+//  Copyright © 2016年 weisheng.wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppCount/AppCount/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AppCount
+//
+//  Created by 王炜圣 on 16/2/25.
+//  Copyright © 2016年 weisheng.wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
